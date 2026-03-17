6c2673c81f3e3096331575175b111d45fe36236d
Ildebrando <ildebrando.s@gmail.com>
2026-03-17 16:27:30 -0300

    Add basic service worker

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'templo-cache-v1';
+
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// instala e salva no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// responde com cache ou rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
